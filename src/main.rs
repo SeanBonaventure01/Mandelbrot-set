c7398e2793ab457ee2d4d8b1d1169cf0d704d106
Sean Bonaventure <spb354@gmail.com>
2025-12-29 00:07:38 -0500

    Added winit, create basic window at start

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,59 @@
-pub mod mandelbrot;
-use num_complex::Complex;
+//pub mod mandelbrot;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowId};
+use winit::dpi::LogicalSize;
 
+const WIDTH: u32 = 640;
+const HEIGHT: u32 = 480;
+
+struct MandelbrotApp{
+    window: Option<Window>
+}
+
+impl MandelbrotApp {
+    pub fn new() -> Self
+    {
+        MandelbrotApp {
+            window: None
+        }
+    }
+}
+
+impl ApplicationHandler for MandelbrotApp {
+    // Required methods
+    fn resumed(&mut self, event_loop: &ActiveEventLoop)
+    {
+        let window_attributes = Window::default_attributes()
+            .with_title("Mandelbrot Set Generator")
+            .with_inner_size(winit::dpi::LogicalSize::new(WIDTH, HEIGHT))
+            .with_resizable(false)
+            .with_blur(true);
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    )
+    {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Close was requested; stopping");
+                event_loop.exit();
+            },
+            _ => {
+                println!("Unhandled event: {:?}", event);
+            },
+        }
+    }
+}
 fn main() 
 {
-    let z1 = Complex::new(0.1, 0.1);
-    let z2 = Complex::new(1.0, 2.0);
-
-    let res_1 = mandelbrot::is_in_mandlebrot_set(z1, 25); 
-    let res_2 = mandelbrot::is_in_mandlebrot_set(z2, 25); 
-    println!("Res 1: {:?}, Res 2: {:?}", res_1, res_2);
+    let event_loop = EventLoop::new().unwrap();
+    let mut mandelbrot_app = MandelbrotApp::new();
+    let _ = event_loop.run_app(&mut mandelbrot_app);
 }
