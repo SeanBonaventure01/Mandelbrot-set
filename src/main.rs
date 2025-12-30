bfd7719bb57f0f51d9eb4f35909a6753300b0586
Sean Bonaventure <spb354@gmail.com>
2025-12-29 20:49:00 -0500

    Mandelbrot rendering working, basic threading working. But threading is slower than normal operation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//pub mod mandelbrot;
+pub mod mandelbrot;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
@@ -6,13 +6,19 @@ use winit::window::{Window, WindowId};
 use winit::dpi::LogicalSize;
 use pixels::Pixels;
 use pixels::SurfaceTexture;
+use std::rc::Rc;
+use std::sync::Arc;
+use std::time::Instant;
 
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
 struct MandelbrotApp<'a>{
-    window: Option<Window>,
-    pixel_buff: Option<Pixels<'a>>
+    window: Option<Arc<Window>>,
+    pixel_buff: Option<Pixels<'a>>,
+    window_width: u32,
+    window_height: u32,
+    mandelbrot_values: Vec<Option<i32>>,
 }
 
 impl<'a> MandelbrotApp<'a> {
@@ -20,21 +26,49 @@ impl<'a> MandelbrotApp<'a> {
     {
         MandelbrotApp {
             window: None,
-            pixel_buff: None
+            pixel_buff: None,
+            window_width: 1280,
+            window_height: 960,
+            mandelbrot_values: vec![None; 0]
         }
     }
+    
+    pub fn with_resolution(&mut self, width: u32, height: u32) {
+        self.window_width = width;
+        self.window_height = height;
+    }
+
+    pub fn update_mandelbrot_values(&mut self, new_values : Vec<Option<i32>>)
+    {
+        self.mandelbrot_values = new_values;
+    }
+
 
     pub fn draw(&mut self) 
     {
         println!("Drawing");
+        if self.mandelbrot_values.len() != (self.window_width*self.window_height) as usize
+        {
+            println!("Inalid length!");
+        }
 
         if let Some(buff) = &mut self.pixel_buff {
                 // Clear the pixel buffer
                 //let frame = buff.frame_mut();
-                for pixel in buff.frame_mut().chunks_exact_mut(4) {
-                    pixel[0] = 0x00; // R
-                    pixel[1] = 0x00; // G
-                    pixel[2] = 0x00; // B
+                for (index, pixel) in buff.frame_mut().chunks_exact_mut(4).enumerate() {
+                    match self.mandelbrot_values[index as usize] {
+                        None => {
+                            pixel[0] = 0x00;
+                            pixel[1] = 0x00;
+                            pixel[2] = 0x00;
+                        },
+                        Some(value) => {
+                            pixel[0] = 0x00;
+                            pixel[1] = 0x00;
+                            pixel[2] = 0xff;
+                        }
+                    }
+
                     pixel[3] = 0xff; // A
                 }
                 buff.render();
@@ -46,14 +80,16 @@ impl<'a> ApplicationHandler for MandelbrotApp<'a> {
     // Required methods
     fn resumed(&mut self, event_loop: &ActiveEventLoop)
     {
+        let inner_size = winit::dpi::PhysicalSize::new(self.window_width, self.window_height);
+        // Todo: Figure out how dpi actually works
         let window_attributes = Window::default_attributes()
             .with_title("Mandelbrot Set Generator")
-            .with_inner_size(winit::dpi::LogicalSize::new(WIDTH, HEIGHT))
+            .with_inner_size(inner_size)
             .with_resizable(false)
             .with_blur(true);
-        let window = event_loop.create_window(window_attributes).unwrap();
-        
-        self.pixel_buff = Some(Pixels::new(WIDTH, HEIGHT, SurfaceTexture::new(WIDTH, HEIGHT, window)).unwrap());
+        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+        self.window = Some(Arc::clone(&window));
+        self.pixel_buff = Some(Pixels::new(inner_size.width, inner_size.height, SurfaceTexture::new(inner_size.width, inner_size.height, window)).unwrap());
         self.draw();
     }
 
@@ -82,5 +118,19 @@ fn main()
 {
     let event_loop = EventLoop::new().unwrap();
     let mut mandelbrot_app = MandelbrotApp::new();
+    let start = Instant::now();
+    let params = mandelbrot::MandelbrotRenderParams::new(
+         mandelbrot_app.window_width,
+         mandelbrot_app.window_height,
+         3.0,
+         2.0,
+         -0.5,
+         0.0,
+         50);
+
+    //mandelbrot_app.update_mandelbrot_values(mandelbrot::compute_set_vals_multithreaded(params, 1));
+    mandelbrot_app.update_mandelbrot_values(mandelbrot::compute_set_vals_naive(params));
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
     let _ = event_loop.run_app(&mut mandelbrot_app);
 }
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,4 +1,5 @@
 use num_complex::Complex;
+use std::thread;
 
 pub fn is_in_mandlebrot_set(complex_num: Complex<f64>, max_steps: i32) -> Option<i32> {
     let mut z = Complex::new(0.0, 0.0);
@@ -13,6 +14,79 @@ pub fn is_in_mandlebrot_set(complex_num: Complex<f64>, max_steps: i32) -> Option
         }
     }
     None
+}
+
+pub struct MandelbrotRenderParams {
+    pixel_width: u32, 
+    pixel_height: u32,
+    mandelbrot_width: f64,
+    mandelbrot_height: f64,
+    center_x: f64,
+    center_y: f64,
+    steps: i32
+}
+
+impl MandelbrotRenderParams {
+    pub fn new(pixel_width: u32, pixel_height: u32, mandelbrot_width: f64, mandelbrot_height: f64,
+        center_x: f64, center_y: f64, steps: i32) -> Self {
+        MandelbrotRenderParams {
+            pixel_width,
+            pixel_height,
+            mandelbrot_width,
+            mandelbrot_height,
+            center_x,
+            center_y,
+            steps}
+    }
+}
+
+pub fn compute_set_vals_multithreaded(params: MandelbrotRenderParams, num_threads: u32) -> Vec<Option<i32>> {
+    let num_rows_per_thread = params.pixel_height/num_threads;
+    let mut handles = vec![];
+
+    for i in 0..num_threads
+    {
+        let start_index = params.pixel_width*i*num_rows_per_thread;
+        let size = num_rows_per_thread;
+        //let new_center_y = (i - num_threads/2)
+        let chunk_params = MandelbrotRenderParams {
+            pixel_width: params.pixel_width,
+            pixel_height: num_rows_per_thread,
+            mandelbrot_width: params.mandelbrot_width,
+            mandelbrot_height: params.mandelbrot_height/(num_threads as f64),
+            center_x: params.center_x,
+            center_y: params.center_y,
+            steps: 100
+        };
+
+        let handle = thread::spawn(move || {
+            compute_set_vals_naive(chunk_params)
+        });
+
+        handles.push(handle);
+    }
+
+    handles.into_iter()
+        .flat_map(|h| h.join().unwrap())
+        .collect()
+}
+
+pub fn compute_set_vals_naive(params: MandelbrotRenderParams) -> Vec<Option<i32>> {
+    let mut mandelbrot_out : Vec<Option<i32>> = vec![None; (params.pixel_width*params.pixel_height) as usize];
+    let center_point_x : i32 = (params.pixel_width as i32)/2;
+    let center_point_y : i32 = (params.pixel_height as i32)/2;
+    let scale_per_pixel_x = params.mandelbrot_width/(params.pixel_width as f64);
+    let scale_per_pixel_y = params.mandelbrot_height/(params.pixel_height as f64);
+
+    for x in 0..params.pixel_width {
+        for y in 0..params.pixel_height {
+            let x_pos : f64 = (((x as i32 - center_point_x) as f64)*scale_per_pixel_x) + params.center_x;
+            let y_pos : f64 = (((y as i32 - center_point_y) as f64)*scale_per_pixel_y) + params.center_y;
+            let complex_val = Complex::new(x_pos, y_pos);
+            mandelbrot_out[(x + y*params.pixel_width) as usize] = is_in_mandlebrot_set(complex_val, params.steps);
+        }
+    }
+    mandelbrot_out
 }
 
 #[cfg(test)]
